ed081932070da9cc209c7c347e0062baf7cf2d81
jailson <jailsonpacagnans@hotmail.com>
2020-10-15 09:46:55 -0300

    added iframe admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,2 +1,16 @@
-import redirect from 'nextjs-redirect'
-export default redirect('https://fabianocorreasite.web.app/admin')
+import Iframe from 'react-iframe'
+
+export default function Admin(){
+    return(
+        <Iframe url="https://fabianocorreasite.web.app/admin"
+            position="absolute"
+            width="100%"
+            id="myId"
+            className="myClassname"
+            height="100%"
+            //styles={{height: "25px"}}
+            />
+    )
+}
+//import redirect from 'nextjs-redirect';
+//export default redirect('https://fabianocorreasite.web.app/admin')
